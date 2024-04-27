0ebf25350b4786e0ea5495efa1e3f44fe828856b
Jalen Murray <jalenmmurray@gmail.com>
2024-04-26 20:07:06 -0400

    Added Dynamic Pages

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -25,7 +25,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
         # If any error occurs
         print("Network exception occurred")
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -83,6 +83,8 @@ def get_cars(request):
     if(count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
+    print("IN GET_CARS")
+    print(car_models)
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
